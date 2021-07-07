7621b7848519ee23f700f447fa05abeb5e3b7081
HENRY Christophe <christophe.henry2@soprabanking.com>
2021-07-07 17:06:43 +0200

    Setup menu after animation

--- src/main/resources/files/js/anim.js
+++ src/main/resources/files/js/anim.js
@@ -1,3 +1,7 @@
 function moveLogoToCenter() {
     document.getElementById('logoWrapper').className = 'moveLogo';
+    setTimeout(function (){
+        document.getElementById('menuWrapper').style.visibility = 'visible'
+        document.getElementById('menuWrapper').className = 'menu';
+    }, 5100);
 }
